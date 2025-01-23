7d8f920b552eb1d47a189df19cc08f35889f0d51
PRINCE BHADANIA <princebhadania007@gmail.com>
2025-01-23 22:04:08 +0530

    Add files via upload

---
+++ Python Theory/04_datatypes.py
@@ -0,0 +1,12 @@
+a = 1 # integer
+b = 5.22 # floating point number
+c = "Apprentice" # string
+d = False # boolean variable
+e = None # none type variable
+
+
+#explicit type casting
+a = "31.2"
+b = float(a) # a but the type should be float
+t = type(b) 
+print(t)
